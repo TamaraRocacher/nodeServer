3d72e562ae5c9cb8fe63059af98f1c9c0da152ac
Tamara Rocacher <tamara.rocacher@gmail.com>
2016-01-01 17:56:35 +0100

    fic

---
+++ app2.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 80);
+
+app.use(express.static(__dirname));
+
+
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname+ '/index.html');
+});
+
+app.get('/cv', function(req,res) {
+    res.sendFile(__dirname + '/cv.html');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Start on port'+ app.get('port'));
+});
