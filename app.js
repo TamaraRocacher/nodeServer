f4e28030ba0b5fd87badcad660642f099355c115
tamara <tamara.rocacher@gmail.com>
2016-02-13 17:06:14 +0100

    css

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 var express = require ('express');
+var compression = require('compression');
 var app = express();
+var fs = require('fs');
+var forceDomain = require('forcedomain');
 
 app.set('port', process.env.PORT || 80);
 
 app.use(express.static(__dirname +'/public' ));
+
+/*app.use(forceDomain({
+    hostname: 'www.tamara.rocacher.com'
+}));*/
+
+app.use(compression());
+
+app.use(function(req, res, next) {
+    var log = fs.createWriteStream('auth.log', {
+	'flags': 'a'
+    });
+    
+    var info = new Date() + ' | ' + req.connection.remoteAddress + ' | ' +
+	req.url + ' | ' + req.method ;
+    
+    /*if (req.session.user) {
+      info += ' | ' + req.session.user.email;
+      }*/
+    
+    console.log(info);
+    log.write(info + '\n');
+    log.end();
+    next();
+});
+
 
 
 app.get('/', function(req,res) {
@@ -16,6 +44,10 @@ app.get('/cv', function(req, res){
 
 app.get('/contact', function(req, res){
     res.sendFile(__dirname + '/pages/contact.html');
+});
+
+app.get('/projet', function(req, res){
+    res.sendFile(__dirname + '/pages/projet.html');
 });
 
 app.listen(app.get('port'),function() {
